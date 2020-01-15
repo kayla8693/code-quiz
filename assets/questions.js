a936a5bcc6a91c9994a03aba005d7e153adbab60
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-15 11:25:28 -0600

    added code files + questions object

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets", "all of the above"],
+      answer: "parentheses"
+    },
+    {
+        title: "What html element holds the Javascript?",
+        choices: ["<body>", "<script>", "<link>", "<head>", "none of the above"],
+        answer: "<script>"
+    },
+    {
+        title: "What tool is used to print content to the debugger?",
+        choices: ["console.log", "function()", "alerts", "if/else statements", "none of the above"],
+        answer: "console.log"
+    },
+    {
+        title: "Which of the following uses correct JavaScript syntax?",
+        choices: ["console,log(arr)", "function{} ", "if {confirmVegetarian}", "for(i=0; i<arr.length; i++)", "none of the above"],
+        answer: "for(i=0; i<arr.length; i++)"
+    },
+  ];
