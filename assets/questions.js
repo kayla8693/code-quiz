349a556cc2c2cac1a3943f373d384070712d9836
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-01-23 17:32:06 -0600

    finished quiz

--- assets/questions.js
+++ assets/questions.js
@@ -1,27 +1,27 @@
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets", "all of the above"],
-      answer: "parentheses"
-    },
-    {
-        title: "What html element holds the Javascript?",
-        choices: ["<body>", "<script>", "<link>", "<head>", "none of the above"],
-        answer: "<script>"
-    },
-    {
-        title: "What tool is used to print content to the debugger?",
-        choices: ["console.log", "function()", "alerts", "if/else statements", "none of the above"],
-        answer: "console.log"
-    },
-    {
-        title: "Which of the following uses correct JavaScript syntax?",
-        choices: ["console,log(arr)", "function{} ", "if {confirmVegetarian}", "for(i=0; i<arr.length; i++)", "none of the above"],
-        answer: "for(i=0; i<arr.length; i++)"
-    },
-  ];
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["A. strings", "B. booleans", "C. alerts", "D. numbers"],
+    answer: "C. alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["A. quotes", "B. curly brackets", "C. parentheses", "D. square brackets"],
+    answer: "C. parentheses"
+  },
+  {
+    title: "What html element holds the Javascript?",
+    choices: ["A. <body>", "B. <script>", "C. <link>", "D. <head>"],
+    answer: "B. <script>"
+  },
+  {
+    title: "What tool is used to print content to the debugger?",
+    choices: ["A. console.log", "B. function()", "C. alerts", "D. if/else statements"],
+    answer: "A. console.log"
+  },
+  {
+    title: "Which of the following uses correct JavaScript syntax?",
+    choices: ["A. console,log(arr)", "B. function{} ", "C. if {confirmVegetarian}", "D. for(i=0; i<arr.length; i++)"],
+    answer: "D. for(i=0; i<arr.length; i++)"
+  }
+];
---
+++ assets/script.js
@@ -0,0 +1,165 @@
+var body = document.body;
+
+var quizBox = document.querySelector(".quiz-box");
+var timeEl = document.querySelector(".time");
+var quizBox = document.querySelector(".quiz-box");
+var startButton = document.querySelector(".start");
+var questionText = document.getElementById("question-text");
+var next = document.getElementsByClassName("next");
+var feedback = document.getElementById("feedback");
+var scoreEl = document.getElementById("score");
+var finalScoreEl = document.getElementById("final-score");
+var endBox = document.getElementById("end-box");
+var submitBtn = document.getElementById("submit");
+var highscoreNav = document.querySelector(".highscore-nav");
+var highscoreBox = document.getElementById("highscore-box");
+var userScore = document.querySelector("#user-score");
+var goBackBtn = document.querySelector("#go-back");
+var clearHighscore = document.querySelector("#clear");
+var title = document.querySelector(".title");
+
+var choice1El = document.getElementById("choice-1");
+var choice2El = document.getElementById("choice-2");
+var choice3El = document.getElementById("choice-3");
+var choice4El = document.getElementById("choice-4");
+
+var secondsLeft = 75;
+var currentQuestionIndex = 0;
+var score = 0;
+
+
+// start game w/ startButton click
+
+startButton.addEventListener("click", function() {
+
+//   hide + reveal relevent elements
+  title.classList.add("hide");
+  startButton.classList.add("hide");
+  quizBox.classList.remove("hide");
+  scoreEl.classList.remove("hide");
+  currentQuestionIndex = 0;
+
+// judges answer correct or incorrect + moves user to next question along w/ nextquestion function
+  function moveOn(userChoice) {
+
+    correctAnswers = questions[currentQuestionIndex].answer;
+
+    if (userChoice === correctAnswers) {
+      console.log("Correct");
+      feedback.setAttribute(
+        "style",
+        "color: green"
+      );
+      feedback.textContent = ("Correct!");
+      score++;
+      scoreEl.textContent = ("Answers Correct: ") + score;
+      console.log(score);
+      console.log(userChoice);
+    }
+    else {
+      console.log("Wrong");
+      feedback.setAttribute(
+        "style",
+        "color: red"
+      );
+      feedback.textContent = ("Wrong!");
+      secondsLeft-=10 ;
+    };
+    
+    currentQuestionIndex++;
+    getNewQuestion(currentQuestionIndex);
+  };
+
+// shows question + choices
+  function getNewQuestion() {
+    var question = questions[currentQuestionIndex];
+    var title = question.title;
+    questionText.textContent = title;
+
+    for (var i = 0; i < next.length; i++) {
+      var choice1 = question.choices[i]
+      next[i].textContent = choice1;
+    };
+
+  };
+
+//   click + move on moves user to next question
+    function nextQuestion() {
+
+      choice1El.addEventListener("click", function () {
+        moveOn(choice1El.textContent)
+      });
+      choice2El.addEventListener("click", function () {
+        moveOn(choice2El.textContent);
+      });
+      choice3El.addEventListener("click", function () {
+        moveOn(choice3El.textContent);
+      });
+      choice4El.addEventListener("click", function () {
+        moveOn(choice4El.textContent);
+      });
+
+    };
+
+// sets timer, + reveals endBox, highscoreBox; saves user info to local storage + inputs it to highscoreBox
+  function setTime() {
+
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
+
+      if(secondsLeft <= 0 || questions.length < currentQuestionIndex + 1) {
+        var finalScore = (secondsLeft * 2) + score;
+
+        clearInterval(timerInterval);
+        quizBox.classList.add("hide");
+        endBox.classList.remove("hide");
+        endBox.setAttribute("style", "color: green");
+
+        if (score === 0) {
+            finalScore = "0";
+            finalScoreEl.textContent = ("Your final score is: " + finalScore);
+        }
+        else {
+
+        finalScoreEl.textContent = ("Your final score is: ") + finalScore;
+        }
+      };
+
+      submitBtn.addEventListener("click", function () {
+            endBox.classList.add("hide");
+            highscoreBox.classList.remove("hide");
+
+            var initials = document.querySelector("#initials").value
+
+            localStorage.getItem("initials")
+            localStorage.getItem("score");
+            localStorage.setItem("initials", initials);
+            localStorage.setItem("score", finalScore);
+
+            userScore.textContent = initials + " = " + finalScore;
+      });
+
+    //   clears highscoreBox
+      clear.addEventListener("click", function () {
+        userScore.textContent = "";
+      });
+
+    }, 1000);
+  };
+
+//   calls all functions
+  nextQuestion();
+  setTime();
+  getNewQuestion();
+});
+
+// reveals highscoreBox
+highscoreNav.addEventListener("click", function () {
+    title.classList.add("hide");
+    quizBox.classList.add("hide");
+    startButton.classList.add("hide");
+    endBox.classList.add("hide");
+    highscoreBox.classList.remove("hide");
+    
+})
